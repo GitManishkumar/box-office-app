946b763c58db93492fbe6a5b11fff201128dfdc9
Manish Kumar <manishkumar96715@gmail.com>
2023-06-25 09:38:52 +0530

    displayed shows and actors data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react';
 import { searchForShows, searchForPeople } from '../api/tvmaze';
-import SearchForm from '../components/SearchForm'
+import SearchForm from '../components/SearchForm';
+import ShowGrid from '../components/shows/ShowGrid';
+import ActorsGrid from '../components/actors/ActorsGrid';
 
 const Home = () => {
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
 
-  
-
-  const onSearch = async({q, searchOption})=> {
-
+  const onSearch = async ({ q, searchOption }) => {
     try {
       setApiDataError(null);
 
-      let result
+      let result;
 
       if (searchOption == 'shows') {
         result = await searchForShows(q);
@@ -21,11 +20,9 @@ const Home = () => {
         result = await searchForPeople(q);
       }
       setApiData(result);
-
     } catch (error) {
       setApiDataError(error);
     }
-
   };
 
   const renderApiData = () => {
@@ -33,19 +30,23 @@ const Home = () => {
       return <div>Error Ocuured:{apiDataError.message}</div>;
     }
 
+    if(apiData?.length===0){
+      return <div>No result found</div>
+    }
+
     if (apiData) {
-      return apiData[0].show
-        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
-        : apiData.map(data => (
-            <div key={data.person.id}>{data.person.name}</div>
-          ));
+      return apiData[0].show ? (
+        <ShowGrid shows={apiData} />
+      ) : (
+        <ActorsGrid actors={apiData} />
+      );
     }
     return null;
   };
 
   return (
     <div>
-      <SearchForm onSearch={onSearch}/>
+      <SearchForm onSearch={onSearch} />
       {/* <form onSubmit={onSearch}>
         <input type="text" value={searchStr} onChange={onSearchInputChange} />
 
